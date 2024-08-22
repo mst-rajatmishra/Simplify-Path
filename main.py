505e24b8cacf607079f629b79d11704c0fc81056
Rajat Mishra <77861069+mst-rajatmishra@users.noreply.github.com>
2024-08-22 12:08:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+
+        parts = path.split('/')
+        
+
+        stack = []
+        
+        for part in parts:
+            if part == '..':
+
+                if stack:
+                    stack.pop()
+            elif part and part != '.':
+
+                stack.append(part)
+        
+
+        return '/' + '/'.join(stack)
